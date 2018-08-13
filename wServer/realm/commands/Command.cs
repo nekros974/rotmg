8c6c5f8894e7317f64ee93cce2710878139a8b30
nekros974 <nekrosincoding@gmail.com>
2018-08-13 06:18:17 +0000

    Initial commit

---
+++ wServer/realm/commands/Command.cs
@@ -0,0 +1,112 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using log4net;
+using wServer.realm.entities.player;
+
+#endregion
+
+namespace wServer.realm.commands
+{
+    public abstract class Command
+    {
+        internal class Ranks
+        {
+            public static readonly int Owner = 5;
+            public static readonly int Dev = 4;
+            public static readonly int Admin = 3;
+            public static readonly int Mod = 2;
+            public static readonly int Donor = 1;
+
+        }
+
+        public Command(string name, int permLevel = 0)
+        {
+            CommandName = name;
+            PermissionLevel = permLevel;
+        }
+
+        public string CommandName { get; private set; }
+        public int PermissionLevel { get; private set; }
+
+        protected abstract bool Process(Player player, RealmTime time, string[] args);
+
+        private static int GetPermissionLevel(Player player)
+        {
+            return player.Client.Account.Rank;
+        }
+
+
+        public bool HasPermission(Player player)
+        {
+            if (GetPermissionLevel(player) < PermissionLevel)
+                return false;
+            return true;
+        }
+
+        public bool Execute(Player player, RealmTime time, string args)
+        {
+            if (!HasPermission(player))
+            {
+                player.SendError("No permission!");
+                return false;
+            }
+
+            try
+            {
+                string[] a = args.Split(' ');
+                return Process(player, time, a);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error when executing the command.", ex);
+                player.SendError("Error when executing the command.");
+                return false;
+            }
+        }
+    }
+
+    public class CommandManager
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof (CommandManager));
+
+        private readonly Dictionary<string, Command> cmds;
+
+        private RealmManager manager;
+
+        public CommandManager(RealmManager manager)
+        {
+            this.manager = manager;
+            cmds = new Dictionary<string, Command>(StringComparer.InvariantCultureIgnoreCase);
+            Type t = typeof (Command);
+            foreach (Type i in t.Assembly.GetTypes())
+                if (t.IsAssignableFrom(i) && i != t)
+                {
+                    Command instance = (Command) Activator.CreateInstance(i);
+                    cmds.Add(instance.CommandName, instance);
+                }
+        }
+
+        public IDictionary<string, Command> Commands
+        {
+            get { return cmds; }
+        }
+
+        public bool Execute(Player player, RealmTime time, string text)
+        {
+            int index = text.IndexOf(' ');
+            string cmd = text.Substring(1, index == -1 ? text.Length - 1 : index - 1);
+            string args = index == -1 ? "" : text.Substring(index + 1);
+
+            Command command;
+            if (!cmds.TryGetValue(cmd, out command))
+            {
+                player.SendError("Unknown command!");
+                return false;
+            }
+            log.InfoFormat("[Command] <{0}> {1}", player.Name, text);
+            return command.Execute(player, time, args);
+        }
+    }
+}
---
+++ wServer/realm/commands/log.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wServer.realm.commands
+{
+    internal class log
+    {
+        internal static void Error(string v, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
